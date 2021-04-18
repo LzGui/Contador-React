5ab8a64b54e01cc69b9667689b655aef85226c80
LzGui <lzguilhermecp@gmail.com>
2021-04-18 15:06:38 -0300

    Botões de incremento e decremento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const styles = {
+  display:{
+    margin: 5
+  }
+}
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  function increment(){
+    setCount(count + 1);
+  }
+
+  function decrement(){
+    setCount(count - 1);
+  }
+
+  useEffect(() => {
+    if (count < 0){
+      alert("Negativo!");
+    }
+  }, [count])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">{count}</div>
+
+      <button onClick={increment} style={styles.button}>+</button>
+      <button onClick={decrement} style={styles.button}>-</button>
+      
     </div>
   );
 }
